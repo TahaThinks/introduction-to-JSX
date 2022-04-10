4b0e690fcefef4d0a4b5c38c739e55251065eb9b
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-04-10 16:05:29 +0400

    Add Multiple input in DOM using render method The render method that manipulate the DOM using React requires 2 inputs and one optional callback function.
    The first input is what we want to add and the second input is where we want to add it.
    
    In what to add we can only add one input, if we want more we wrap it in a DIV

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a Paragraph</p>
+  </div>,
+  document.getElementById("root")
+  );
 
-ReactDOM.render(<h1>Hello World!</h1>,document.getElementById("root"));
