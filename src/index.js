62cac5aaa2432b0121edbd7cf06b5e28e63a3392
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-04-10 15:50:21 +0400

    Inserting to DOM from a JS File

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "../public/styles.css";
+var React = require("react");
+var ReactDOM = require("react-dom");
 
-var today = new Date();
-var time = today.getHours();
-var string = null;
 
-const customStyle = {
-  color: ""
-};
-
-if (time > 0 && time < 12) {
-  string = "Morning";
-  customStyle.color = "red";
-} else if (time > 12 && time < 6) {
-  string = "Afternoon";
-  customStyle.color = "red";
-} else {
-  string = "Evening";
-  customStyle.color = "red";
-}
-
-ReactDOM.render(
-  <h1 className="heading" style={customStyle}>
-    Good {string}
-  </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<h1>Hello World!</h1>,document.getElementById("root"));
